99a1565647de973f5ed4f1f064df2b0f2f9a7165
Zihni Ugurbil <zihni.ugurbil@gmail.com>
2025-09-04 10:21:08 -0400

    changed loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 2; i <= 6; i++) {
             System.out.println("i = " + i);
         }
     }
